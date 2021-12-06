3f958966538875f2829b04a0c7bd70b57672204b
Trinity Fore <trinityfore@Trinitys-MacBook-Air.local>
2021-12-06 16:45:56 -0600

    adds submit button to form

--- models/products.js
+++ models/products.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose')
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema ({
-    name:String, required: true ,
+    name:String,
     description: String,
     img: String,
     price: Number,
